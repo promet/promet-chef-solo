74b3fb26be7e776777cf7c41c6fc33a53c6ed0b7
Greg Palmier <gregpalmier@gmail.com>
2013-06-27 13:15:35 -0500

    newrelic server monitoring added to default lamp deployments

--- roles/newrelic.rb
+++ roles/newrelic.rb
@@ -6,5 +6,12 @@ run_list(
 )
 
 default_attributes(
-    :newrelic => { :license_key => "014ff90b4e2bb88affd4bd72271f488e9cc422f7" }
+  :newrelic => { 
+    :server_monitoring => { 
+      :license => "014ff90b4e2bb88affd4bd72271f488e9cc422f7"
+    },
+    :application_monitoring => {
+      :license => "014ff90b4e2bb88affd4bd72271f488e9cc422f7"
+    }
+   }
 )

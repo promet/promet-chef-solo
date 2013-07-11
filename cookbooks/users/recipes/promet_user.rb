c7b33cea2747a54be926ff6f79d79828c48abf59
Greg Palmier <gregpalmier@gmail.com>
2013-07-11 13:15:32 -0500

    add git resource checkout to promet_user

--- cookbooks/users/recipes/promet_user.rb
+++ cookbooks/users/recipes/promet_user.rb
@@ -3,4 +3,12 @@ cookbook_file "/home/promet/.ssh/id_rsa" do
  owner "promet"
  group "dev"
  mode 0600
+ action :create
+end
+
+git "/home/promet/scriptutils" do
+   repository "git@git.promethost.com:scriputils"
+   action :checkout
+   user "promet"
+   group "dev"
 end

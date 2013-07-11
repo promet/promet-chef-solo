e18efb81bdaa431897bd5d76ac5576c0ab4b9411
Greg Palmier <gregpalmier@gmail.com>
2013-07-11 18:35:40 -0500

    clean up nagios ssl and database user creds for mysql auth

--- roles/nagios-server.rb
+++ roles/nagios-server.rb
@@ -12,6 +12,14 @@ default_attributes(
     "server_auth_method" => "htauth",
     "server_role" => "nagios-server",
     "notifications_enabled" => "1",
-    "users_databag_group" => "ops"
+    "users_databag_group" => "ops",
+    "enable_ssl" => "true",
+    "server_name" => "monitoring-promet-rsc.promethost.com",
+    "ssl_cert_file" => "/etc/nagios3/certificates/nagios-server.crt",
+    "ssl_cert_key" => "/etc/nagios3/certificates/nagios-server.key"
+#node['nagios']['ssl_cert_file'] = Location of SSL Certificate File. default "/etc/nagios3/certificates/nagios-server.pem"
+#node['nagios']['ssl_cert_key'] = Location of SSL Certificate Key. default "/etc/nagios3/certificates/nagios-server.pem"
+#node['nagios']['server_name'] - common name to use in a server cert, default "nagios"
+#node['nagios']['ssl_req'] - info to
   }
 )

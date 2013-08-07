e22ace51f7192aaa264b6df6094f2555ea508753
Greg Palmier <gregpalmier@gmail.com>
2013-08-07 09:52:08 -0500

    resolve host checks crawling over each other by customization

--- roles/nagios-server.rb
+++ roles/nagios-server.rb
@@ -11,10 +11,20 @@ default_attributes(
   "nagios" => { 
     "server_auth_method" => "htauth",
     "server_role" => "nagios-server",
-    "notifications_enabled" => "0",
+    "notifications_enabled" => "1",
     "users_databag_group" => "ops",
     "enable_ssl" => "true",
     "server_name" => "nagios-rsc.promethost.com",
+    "default_host" => {
+    	"check_interval" => "60",
+    	"retry_interval" => "20",
+    	"max_check_attempts" => "3"
+    	},
+    "default_service" => {
+    	"check_interval" => "60",
+    	"retry_interval" => "20",
+    	"max_check_attempts" => "3"
+	},
     "ssl_cert_file" => "/etc/nagios3/certificates/nagios-server.crt",
     "ssl_cert_key" => "/etc/nagios3/certificates/nagios-server.key"
 #node['nagios']['ssl_cert_file'] = Location of SSL Certificate File. default "/etc/nagios3/certificates/nagios-server.pem"

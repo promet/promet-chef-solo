f4447e99cdb66c0cdda4dcfc456c7a80430bc6b5
Gerold Mercadero <gmercadero@gmail.com>
2013-11-19 23:02:44 +0800

    add custom_arcsweb

--- roles/customizations/custom_arcsweb.rb
+++ roles/customizations/custom_arcsweb.rb
@@ -1,12 +1,17 @@
 name "custom_arcsweb"
-description "ARCS Web customizations"
+description "ARCS Web Customizations"
 run_list(
 )
 
 override_attributes(
-  "promet_php" => 
-   { "apc" => 
-     { "shm_size" => '192M'
-     }
-   }
+  "promet_php" => {
+    "apc" => {
+      "shm_size" => '192M'
+    },
+    "memory_limit" => "256M",
+    "upload_max_filesize" => "50M",
+    "post_max_size" => "50M",
+    "max_execution_time" => "600",
+    "max_input_time" => "600"
+  }
 )

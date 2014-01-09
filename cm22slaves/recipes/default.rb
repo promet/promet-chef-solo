8e7498bcccecb8436b652e086844c4b094080c10
Will Milton <wa.milton@gmail.com>
2014-01-09 16:40:19 -0600

    cookbook for automating cm22 slave deployment

---
+++ cm22slaves/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: cm22slaves
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'git'
+
+node.cm22slave.sites.each do |subdomain, machine_name|
+  root = "/var/www/sites/#{subdomain}.prometdev.com"
+
+  git root do
+    user        'promet'
+    group       'www-data'
+    repository  'git@github.com:promet/CM22Slave'
+  end
+
+  script "update_cm22slave_site-#{machine_name}" do
+    user  'promet'
+    cwd   path
+    code  <<-SCRIPT
+    drush kw-b
+    cd build
+    drush kw-id -y
+    drush vset 'oauth_slave_master_domain' 'http://corporate22cm.prometdev.com'
+    drush cc-aic -y #{machine_name} dev
+    drush kw-u
+    SCRIPT
+  end
+end

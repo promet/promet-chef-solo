4d687977ae1d6a2ba5f5b0f253b1ad0db041a313
Greg Palmier <gregpalmier@gmail.com>
2014-02-03 18:52:42 -0600

    consolidate the ssh_known_hosts recipe to the users since it is a dependency for the "promet" user and has no customizations.

--- roles/ssh_known_hosts.rb
+++
@@ -1,8 +0,0 @@
-name "ssh_known_hosts"
-description "ssh_known_hosts config"
-run_list(
-    "recipe[ssh_known_hosts]"
-)
-
-default_attributes(
-)
--- roles/users.rb
+++ roles/users.rb
@@ -7,7 +7,9 @@ run_list(
     "recipe[users::sysadmins]",
     "recipe[users::www-data]",
     "recipe[users::promet_user]",
-    "recipe[users::remove_users]"
+    "recipe[users::remove_users]",
+    "role[sudo]",
+    "recipe[ssh_known_hosts]"
 )
 
 default_attributes(

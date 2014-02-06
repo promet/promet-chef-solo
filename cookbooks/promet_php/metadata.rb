5a7c793dd0f7f31e66e93c25cbd68d25a1dd2368
Greg Palmier <gregpalmier@gmail.com>
2014-02-06 11:11:51 -0600

    organize the 0.1.0 version of the promet_php cookbook.

--- cookbooks/promet_php/metadata.rb
+++ cookbooks/promet_php/metadata.rb
@@ -1,7 +1,10 @@
 name             'promet_php'
 maintainer       'Promet Solutions, Inc.'
-maintainer_email 'greg@promethost.com'
+maintainer_email 'greg@prometsource.com'
 license          'All rights reserved'
 description      'Installs/Configures promet_php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apache2'
+depends 'php'

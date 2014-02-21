3afbf02ccd00ff1a3bb5c451b961db147ee98e6f
Will Milton <wa.milton@gmail.com>
2014-02-21 16:59:28 -0600

    adding a cm22 slave

--- cookbooks/cm22/attributes/default.rb
+++ cookbooks/cm22/attributes/default.rb
@@ -4,19 +4,20 @@ default.cm22.data_bag
 default.cm22.httpd_group = 'www-data'
 
 default.cm22.slave.sites = {
-  "publication22cm"         => "publication.prometstaging.com",
-  "the_orland_park_prairie" => "opprarie",
-  "the_frankfort_station"   => "frankfortstation",
-  "the_homer_horizon"       => "homerhorizon",
-  "the_lockport_legend"     => "lockportlegend",
-  "the_mokena_messenger"    => "mokenamessenger",
-  "the_new_lenox_patriot"   => "newlenoxpatriot",
-  "the_tinley_junction"     => "tinleyjunction",
-  "the_glenview_lantern"    => "glenviewlantern",
-  "the_northbrook_tower"    => "northbrooktower",
-  "the_wilmette_beacon"     => "wilmettebeacon",
-  "the_winnetka_current"    => "winnetkacurrent",
-  "malibu_surfside_news"    => "malibusurfsidenews"
+  "publication22cm"             => "publication.prometstaging.com",
+  "the_orland_park_prairie"     => "opprarie",
+  "the_frankfort_station"       => "frankfortstation",
+  "the_homer_horizon"           => "homerhorizon",
+  "the_lockport_legend"         => "lockportlegend",
+  "the_mokena_messenger"        => "mokenamessenger",
+  "the_new_lenox_patriot"       => "newlenoxpatriot",
+  "the_tinley_junction"         => "tinleyjunction",
+  "the_glenview_lantern"        => "glenviewlantern",
+  "the_northbrook_tower"        => "northbrooktower",
+  "the_wilmette_beacon"         => "wilmettebeacon",
+  "the_winnetka_current"        => "winnetkacurrent",
+  "the_highland_park_landmark"  => "hplandmark",
+  "malibu_surfside_news"        => "malibusurfsidenews"
 }
 
 default.cm22.slave.process        = []

0290ded8c4723121d64338745bebfd34dd24b588
Will Milton <wa.milton@gmail.com>
2014-02-24 22:13:39 -0600

    updates to cm22 docroots

--- cookbooks/cm22/recipes/vhosts.rb
+++ cookbooks/cm22/recipes/vhosts.rb
@@ -4,7 +4,7 @@ node.cm22.slave.process.each do |name|
   web_app slave do
     template 'web_app.conf.erb'
     cookbook 'apache2'
-    docroot "#{node.cm22.webroot}/#{slave}"
+    docroot "#{node.cm22.webroot}/#{slave}/build"
     server_aliases ["#{slave}.com"]
     server_name "www.#{slave}.com"
     allow_override "All"
@@ -14,7 +14,7 @@ end
 web_app '22ndcenturymedia' do
   template 'web_app.conf.erb'
   cookbook 'cm22'
-  docroot "#{node.cm22.webroot}/22ndcenturymedia"
+  docroot "#{node.cm22.webroot}/cm22master/build"
   server_aliases ["22ndcenturymedia.com"]
   server_name "www.22ndcenturymedia.com"
   allow_override "All"

4c96ce1049bfb1e741a08d99f652df187f895e4b
Greg Palmier <gregpalmier@gmail.com>
2014-03-11 11:52:52 -0500

    remove user cookbook dependencies and updated environments

--- cookbooks/users/metadata.rb
+++ cookbooks/users/metadata.rb
@@ -4,13 +4,10 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.5.4"
+version          "1.5.5"
 recipe           "users", "Empty recipe for including LWRPs"
 recipe           "users::sysadmins", "Create and manage sysadmin group"
 
 %w{ ubuntu debian redhat centos fedora freebsd}.each do |os|
   supports os
 end
-
-depends 'ssh_known_hosts'
-depends 'git'

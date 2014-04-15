df24c5aec616e2c87cabd6fb17fa17a1e909bee9
Greg Palmier <gregpalmier@gmail.com>
2014-04-15 14:07:21 -0500

    establish some basic nginx best practices for starters...

--- roles/nginx.rb
+++ roles/nginx.rb
@@ -5,4 +5,10 @@ run_list(
 )
 
 default_attributes(
+  :nginx => {
+    :server_tokens => "off",
+    :default_site_enabled => false,
+    :keepalive_timeout => 15,
+    :proxy_read_timeout => 20,
+  }
 )

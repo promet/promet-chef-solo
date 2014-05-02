fc3adb8ed4729046e30ca4a4f63e3d1bf3c423fc
Greg Palmier <gregpalmier@gmail.com>
2014-05-02 10:15:12 -0500

    Apply a timezone setting default to PHP installs. clean up spacing, whitespace.

--- roles/php-nginx.rb
+++ roles/php-nginx.rb
@@ -14,13 +14,14 @@ run_list(
 
 default_attributes(
   "promet_php" => {
-        "memory_limit" => "196M",
-        "upload_max_filesize" => "30M",
-        "post_max_size" => "10M",
-        "expose_php" => "Off",
-        "max_execution_time" => "300",
-        "max_input_time" => "300",
-        "realpath_cache_size" => "64k",
-        "conf_dir" => "/etc/php5/apache2",
+    "memory_limit" => "196M",
+    "upload_max_filesize" => "30M",
+    "post_max_size" => "10M",
+    "expose_php" => "Off",
+    "max_execution_time" => "300",
+    "max_input_time" => "300",
+    "realpath_cache_size" => "64k",
+    "date_timezone" => "America/Chicago",
+    "conf_dir" => "/etc/php5/apache2",
   }
 )

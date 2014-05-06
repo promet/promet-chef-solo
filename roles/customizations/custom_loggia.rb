6ce94095461029a63884dc10a84c9820e4db0643
Greg Palmier <gregpalmier@gmail.com>
2014-05-06 15:30:03 -0500

    installed varnish on dev/stage.  config apache for backend use.

--- roles/customizations/custom_loggia.rb
+++ roles/customizations/custom_loggia.rb
@@ -6,6 +6,9 @@ run_list(
 #<%= node[:php][:memory_limit] %>
 #[:php][:apc:][:shm_size]  = "128M"
 override_attributes(
+  "apache" => {
+    "listen_ports" => ["8080", "443"],
+  },
   "promet_php" => {
     "apc" => {
       "shm_size" => '128'
